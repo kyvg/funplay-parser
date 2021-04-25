7ba1c6ec93ebc9e009a1a8614eecd3a00239fd6f
Евгений Кирпичёв <e.kirpichyov@ya.ru>
2021-04-25 20:41:24 +0300

    Fix sendToAll message

--- index.ts
+++ index.ts
@@ -92,10 +92,10 @@ function diff(seller: ParsedItem) {
   if (s) {
     // смотрим менялась ли цена или бьем продажи
     if (s.gold !== seller.gold) {
-      sendToAll(`*${seller.name}*: 💰${s.gold} -> ${seller.gold} \\[ ${compare(s.gold,seller.gold)}/${seller.server}\]\(${seller.rec}\)`);
+      sendToAll(`*${seller.name}*: 💰${s.gold} => ${seller.gold} \\[ ${compare(s.gold,seller.gold)}/${seller.server}\]\(${seller.rec}\)`);
     }
     if (s.price !== seller.price) {
-      sendToAll(`*${seller.name}*: '📈'${s.price} -> ${seller.price}`)
+      sendToAll(`*${seller.name}*: '📈'${s.price} => ${seller.price}`)
     }
   }
 }
